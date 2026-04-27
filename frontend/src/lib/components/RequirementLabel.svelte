0f5dffd152d638b3935a1f8290094234d48d07f0
Abderrahmane Smimite <smimite@gmail.com>
2026-04-27 18:16:31 +0200

    fix test

--- frontend/src/lib/components/RequirementLabel.svelte
+++ frontend/src/lib/components/RequirementLabel.svelte
@@ -7,20 +7,17 @@
 	}
 
 	let { ref_id, name, bold = false, class: className = '' }: Props = $props();
+
+	const sep = $derived(ref_id && /[\p{L}\p{N}]$/u.test(ref_id) ? ' - ' : ' ');
 </script>
 
 {#if ref_id || name}
 	<span class={className}>
-		{#if ref_id}
-			<span
+		{#if ref_id}<span
 				class="inline-block px-1.5 py-0.5 rounded bg-gray-100 text-gray-700 border border-gray-200 font-mono text-xs leading-none align-middle"
-			>
-				{ref_id}
-			</span>
-		{/if}
-		{#if name}
-			{#if ref_id}<span> </span>{/if}
-			<span class={bold ? 'font-semibold' : ''}>{name}</span>
-		{/if}
+				>{ref_id}</span
+			>{/if}{#if ref_id && name}{sep}{/if}{#if name}<span class={bold ? 'font-semibold' : ''}
+				>{name}</span
+			>{/if}
 	</span>
 {/if}
